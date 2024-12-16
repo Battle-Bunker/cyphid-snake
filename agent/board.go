2bc00f8cf1326c35b3ec28162b64fe228c79cc2a
cyphid <146806761+cyphid@users.noreply.github.com>
2024-12-16 11:13:57 +0000

    Assistant checkpoint: Update Cell interface to use rules.Point for coordinates
    Assistant generated file changes:
    - agent/board.go: Update Cell interface and implementations to use rules.Point
    Update Cell interface and implementations to use rules.Point
    Update board initialization to use rules.Point
    Update Cell interface and implementations to use rules.Point
    Update board initialization to use rules.Point
    Update food cell initialization
    Update Cell interface and implementations to use rules.Point
    Update board initialization to use rules.Point
    Update food cell initialization
    Remove NewSnakePartCell function and update snake part initialization
    Update Cell interface and implementations to use rules.Point
    Update board initialization to use rules.Point
    Update food cell initialization
    Remove NewSnakePartCell function and update snake part initialization
    Update snake parts initialization
    Update Cell interface and implementations to use rules.Point
    Update board initialization to use rules.Point
    Update food cell initialization
    Remove NewSnakePartCell function and update snake part initialization
    Update snake parts initialization
    Update snake body initialization
    Update Cell interface and implementations to use rules.Point
    Update board initialization to use rules.Point
    Update food cell initialization
    Remove NewSnakePartCell function and update snake part initialization
    Update snake parts initialization
    Update snake body initialization
    Update snake tail initialization
    
    ---
    
    User prompt:
    
    instead of using a BaseCell with separate X and Y properties, just put Coordinates of type rules.Point in the Cell interface and include that property in each of the implementing structs.

--- agent/board.go
+++ agent/board.go
@@ -1,127 +1,139 @@
-
 package agent
 
 type CellKind int
 
 const (
-  CellEmpty CellKind = iota
-  CellFood
-  CellSnakeHead
-  CellSnakeBody
-  CellSnakeTail
+	CellEmpty CellKind = iota
+	CellFood
+	CellSnakeHead
+	CellSnakeBody
+	CellSnakeTail
 )
 
 type Cell interface {
-  Kind() CellKind
-  IsPassable() bool
+	Kind() CellKind
+	IsPassable() bool
+	Coordinates() rules.Point
 }
 
-type EmptyCell struct{}
+type EmptyCell struct {
+	coordinates rules.Point
+}
 
-func (e EmptyCell) Kind() CellKind { return CellEmpty }
-func (e EmptyCell) IsPassable() bool { return true }
+func (e EmptyCell) Kind() CellKind {
+	return CellEmpty
+}
+func (e EmptyCell) IsPassable() bool {
+	return true
+}
+func (e EmptyCell) Coordinates() rules.Point {
+	return e.coordinates
+}
 
-type FoodCell struct{}
+type FoodCell struct {
+	coordinates rules.Point
+}
 
-func (f FoodCell) Kind() CellKind { return CellFood }
-func (f FoodCell) IsPassable() bool { return true }
+func (f FoodCell) Kind() CellKind {
+	return CellFood
+}
+func (f FoodCell) IsPassable() bool {
+	return true
+}
+func (f FoodCell) Coordinates() rules.Point {
+	return f.coordinates
+}
 
 // We can differentiate snake parts by a named type:
 type SnakePartType int
+
 const (
-  SnakePartHead SnakePartType = iota
-  SnakePartBody
-  SnakePartTail
+	SnakePartHead SnakePartType = iota
+	SnakePartBody
+	SnakePartTail
 )
 
 type SnakePartCell struct {
-  SnakeID      string
-  PartType     SnakePartType
-  // If this is a tail part that will disappear next turn, we can indicate it here:
-  WillVanishNextTurn bool
+	coordinates        rules.Point
+	SnakeID           string
+	PartType          SnakePartType
+	WillVanishNextTurn bool
 }
 
-func (spc SnakePartCell) Kind() CellKind {
-  switch spc.PartType {
-  case SnakePartHead:
-      return CellSnakeHead
-  case SnakePartBody:
-      return CellSnakeBody
-  case SnakePartTail:
-      return CellSnakeTail
-  default:
-      return CellEmpty // should never happen
-  }
+func (s SnakePartCell) Coordinates() rules.Point {
+	return s.coordinates
+}
+func (s SnakePartCell) Kind() CellKind {
+	switch s.PartType {
+	case SnakePartHead:
+		return CellSnakeHead
+	case SnakePartBody:
+		return CellSnakeBody
+	case SnakePartTail:
+		return CellSnakeTail
+	default:
+		return CellEmpty // should never happen
+	}
 }
 
-func (spc SnakePartCell) IsPassable() bool {
-  return spc.PartType == SnakePartTail && spc.WillVanishNextTurn
+func (s SnakePartCell) IsPassable() bool {
+	return s.PartType == SnakePartTail && s.WillVanishNextTurn
 }
 
 type Board struct {
-    Width, Height int
-    Cells         [][]Cell
+	Width, Height int
+	Cells         [][]Cell
 }
 
 func NewBoard(g GameSnapshot) *Board {
-    board := &Board{
-        Width:  g.Width(),
-        Height: g.Height(),
-        Cells:  make([][]Cell, g.Height()),
-    }
+	board := &Board{
+		Width:  g.Width(),
+		Height: g.Height(),
+		Cells:  make([][]Cell, g.Height()),
+	}
 
-    // Initialize cells
-    for y := 0; y < g.Height(); y++ {
-        board.Cells[y] = make([]Cell, g.Width())
-        for x := 0; x < g.Width(); x++ {
-            board.Cells[y][x] = EmptyCell{}
-        }
-    }
+	// Initialize cells
+	for y := 0; y < g.Height(); y++ {
+		board.Cells[y] = make([]Cell, g.Width())
+		for x := 0; x < g.Width(); x++ {
+			board.Cells[y][x] = EmptyCell{coordinates: rules.Point{X: x, Y: y}}
+		}
+	}
 
-    // Place food
-    for _, food := range g.Food() {
-        if food.Y < g.Height() && food.X < g.Width() {
-            board.Cells[food.Y][food.X] = FoodCell{}
-        }
-    }
+	// Place food
+	for _, food := range g.Food() {
+		if food.Y < g.Height() && food.X < g.Width() {
+			board.Cells[food.Y][food.X] = FoodCell{coordinates: food}
+		}
+	}
 
-    // Place snakes
-    for _, snake := range g.Snakes() {
-        body := snake.Body()
-        if len(body) == 0 {
-            continue
-        }
+	// Place snakes
+	for _, snake := range g.Snakes() {
+		body := snake.Body()
+		if len(body) == 0 {
+			continue
+		}
 
-        // Head
-        if body[0].Y < g.Height() && body[0].X < g.Width() {
-            board.Cells[body[0].Y][body[0].X] = SnakePartCell{
-                SnakeID:  snake.ID(),
-                PartType: SnakePartHead,
-            }
-        }
+		// Head
+		if body[0].Y < g.Height() && body[0].X < g.Width() {
+			board.Cells[body[0].Y][body[0].X] = SnakePartCell{coordinates: body[0], SnakeID: snake.ID(), PartType: SnakePartHead}
+		}
 
-        // Body
-        for i := 1; i < len(body)-1; i++ {
-            if body[i].Y < g.Height() && body[i].X < g.Width() {
-                board.Cells[body[i].Y][body[i].X] = SnakePartCell{
-                    SnakeID:  snake.ID(),
-                    PartType: SnakePartBody,
-                }
-            }
-        }
+		// Body
+		for i := 1; i < len(body)-1; i++ {
+			if body[i].Y < g.Height() && body[i].X < g.Width() {
+				board.Cells[body[i].Y][body[i].X] = SnakePartCell{coordinates: body[i], SnakeID: snake.ID(), PartType: SnakePartBody}
+			}
+		}
 
-        // Tail
-        if len(body) > 1 {
-            tail := body[len(body)-1]
-            if tail.Y < g.Height() && tail.X < g.Width() {
-                board.Cells[tail.Y][tail.X] = SnakePartCell{
-                    SnakeID:            snake.ID(),
-                    PartType:           SnakePartTail,
-                    WillVanishNextTurn: snake.Health() < 100 && len(snake.Body()) >= 3,
-                }
-            }
-        }
-    }
+		// Tail
+		if len(body) > 1 {
+			tail := body[len(body)-1]
+			if tail.Y < g.Height() && tail.X < g.Width() {
+				board.Cells[tail.Y][tail.X] = SnakePartCell{coordinates: tail, SnakeID: snake.ID(), PartType: SnakePartTail, WillVanishNextTurn: snake.Health() < 100 && len(snake.Body()) >= 3}
+			}
+		}
+	}
 
-    return board
+	return board
 }
